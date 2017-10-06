16a2b4444335f9cb32f364b4635e3c2ec0d78a79
Rishi Javia <rishi.javia.3895@gmail.com>
2017-10-06 13:31:42 -0700

    (#34) Destroy layer

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
-  
+
   concern :gbl_exportable, Geoblacklight::Routes::Exportable.new
-resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
-  concerns :gbl_exportable
-end
-concern :gbl_wms, Geoblacklight::Routes::Wms.new
-namespace :wms do
-  concerns :gbl_wms
-end
-concern :gbl_downloadable, Geoblacklight::Routes::Downloadable.new
-namespace :download do
-  concerns :gbl_downloadable
-end
-resources :download, only: [:show]
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :gbl_exportable
+  end
+  concern :gbl_wms, Geoblacklight::Routes::Wms.new
+  namespace :wms do
+    concerns :gbl_wms
+  end
+  concern :gbl_downloadable, Geoblacklight::Routes::Downloadable.new
+  namespace :download do
+    concerns :gbl_downloadable
+  end
+  resources :download, only: [:show]
 
   mount Geoblacklight::Engine => 'geoblacklight'
   mount Blacklight::Engine => '/'
   root to: "catalog#index"
-    concern :searchable, Blacklight::Routes::Searchable.new
+  concern :searchable, Blacklight::Routes::Searchable.new
 
   resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
     concerns :searchable
@@ -25,6 +25,8 @@ resources :download, only: [:show]
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   concern :exportable, Blacklight::Routes::Exportable.new
+
+  get '/catalog/destroy/:id' => 'catalog#destroy', :as => :destroy_solr_document
 
   resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
     concerns :exportable
