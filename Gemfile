392d865311ee3cab48e21c0763bb92ab02bd6f89
Rishi Javia <rishi.javia.3895@gmail.com>
2017-10-10 14:09:26 -0700

    (#44) Add figs to use ENV variables

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+# Use figs to store credentials
+gem "figs"
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require 'figs'
+Figs.load()
+
 module GeoLclark
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
