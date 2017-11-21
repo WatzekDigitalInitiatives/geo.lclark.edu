d70d2e67b503c6a274e6646bd928e531f8e0d63d
Rishi Javia <rishi.javia.3895@gmail.com>
2017-11-21 14:45:36 -0800

    (#51) Use https links

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,10 +310,10 @@ Devise.setup do |config|
   # Configure with your SAML settings (see ruby-saml's README for more information: https://github.com/onelogin/ruby-saml).
   config.saml_configure do |settings|
     # assertion_consumer_service_url is required starting with ruby-saml 1.4.3: https://github.com/onelogin/ruby-saml#updating-from-142-to-143
-    settings.assertion_consumer_service_url     = "http://geo.watzek.cloud/users/saml/auth"
+    settings.assertion_consumer_service_url     = "https://geo.watzek.cloud/users/saml/auth"
     settings.assertion_consumer_service_binding = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
     settings.name_identifier_format             = "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified"
-    settings.issuer                             = "http://geo.watzek.cloud/"
+    settings.issuer                             = "https://geo.watzek.cloud/"
     settings.idp_slo_target_url                 = "https://sso.connect.pingidentity.com/sso/SLO.saml2"
     settings.idp_sso_target_url                 = "https://sso.connect.pingidentity.com/sso/idp/SSO.saml2?saasid=8655b747-222d-49d5-9575-2e12b494552d&idpid=7e56e0ab-c5d9-4f27-9044-d8b05854a008"
     settings.idp_cert                           = ENV['IDP_CERT']
