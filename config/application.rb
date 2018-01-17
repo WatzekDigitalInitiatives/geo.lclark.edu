13d28cb8dd6578325aba5ec467874172c24d7735
budak <thatbudakguy@gmail.com>
2018-01-17 14:51:26 -0800

    remove figs

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require 'figs'
-Figs.load()
-
 module Compass
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
